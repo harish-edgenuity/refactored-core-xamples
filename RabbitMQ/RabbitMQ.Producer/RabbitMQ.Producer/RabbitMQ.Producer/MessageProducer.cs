664dbc8e85d94641a65d44c62dee15f79f648bc9
Harish <harish.kumar@excelindia.com>
2024-01-18 17:22:11 +0530

    RabbitMQ-with-.NetCore
    Simple POC on RabbitMQ-with-.NetCOre

---
+++ RabbitMQ/RabbitMQ.Producer/RabbitMQ.Producer/RabbitMQ.Producer/MessageProducer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+ 
+ 
+using System.Text;
+
+namespace RabbitMQ.Producer
+{
+    public class MessageProducer : IMessageProducer
+    {
+        public void SendMessage<T>(T message)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("product", exclusive: false);
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+            //put the data on to the product queue
+            channel.BasicPublish(exchange: "", routingKey: "product", body: body);
+        }
+    }
+}
+   
---
+++ RabbitMQ/RabbitMQ.Producer/RabbitMQ.Producer/RabbitMQ.Producer/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace RabbitMQ.Producer
+{
+    public class Product
+    {
+        public Int64 Id { get; set; }
+        public Guid Guid { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
